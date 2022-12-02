ee63a94d9e60b926d27dd920d51360d8139b7549
Zakhar Borisov <zakhar.borisov@mail.ru>
2022-12-02 15:26:09 +0900

    add some new code with bug

--- node/src/main/java/ru/borisov/service/FileService.java
+++ node/src/main/java/ru/borisov/service/FileService.java
@@ -2,7 +2,9 @@ package ru.borisov.service;
 
 import org.telegram.telegrambots.meta.api.objects.Message;
 import ru.borisov.entity.AppDocument;
+import ru.borisov.entity.AppPhoto;
 
 public interface FileService {
-    AppDocument processDoc(Message externalMessage);
+    AppDocument processDoc(Message telegramMessage);
+    AppPhoto processPhoto(Message telegramMessage);
 }
